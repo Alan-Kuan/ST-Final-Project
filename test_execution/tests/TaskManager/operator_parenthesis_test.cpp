5cd7abced292008adecbc8b48e9ca161b51519be
Alan-Kuan <kuanhaoan@gmail.com>
2023-05-07 21:08:49 +0800

    test: add tests for operator()

---
+++ test_execution/tests/TaskManager/operator_parenthesis_test.cpp
@@ -0,0 +1,31 @@
+#include <gtest/gtest.h>
+#include <vector>
+#include "cppbdd/TaskManager.hpp"
+
+TEST(TestExecution, CallableTaskOperatorParenthesis) {
+    int a = 0;
+    cppbdd::CallableTask task(cppbdd::TaskName::SCENARIO, "", [&]() { a = 1; });
+
+    task();
+
+    EXPECT_EQ(a, 1);
+}
+
+TEST(TestExecution, MultiArgCallableTaskOperatorParenthesis) {
+    std::vector<int> a;
+    cppbdd::MultiArgCallableTask<int>task(
+        cppbdd::TaskName::GIVEN,
+        "a = {}",
+        [&](int x) { a.push_back(x); },
+        std::vector<cppbdd::MultiArgCallableTask<int>::TestCase> {
+            cppbdd::MultiArgCallableTask<int>::TestCase(1),
+            cppbdd::MultiArgCallableTask<int>::TestCase(2),
+        }
+    );
+
+    task();
+    task();
+
+    EXPECT_EQ(a[0], 1);
+    EXPECT_EQ(a[1], 2);
+}
