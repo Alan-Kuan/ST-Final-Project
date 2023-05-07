1ed0b162d979f95715c79655c75c70bc93d6332d
Alan-Kuan <kuanhaoan@gmail.com>
2023-05-07 20:04:45 +0800

    test: add a test for runAll() when there're tasks with multiple arguments

--- test_execution/tests/TaskManager/runAll_test.cpp
+++ test_execution/tests/TaskManager/runAll_test.cpp
@@ -18,3 +18,48 @@ TEST(TestExecutionTaskManager, runAll) {
     EXPECT_EQ(b, 1);
     EXPECT_TRUE(res);
 }
+
+TEST(TestExecutionTaskManager, runAllMultiArgs) {
+    int a, b, c;
+    bool res;
+    cppbdd::TaskManager manager(1);
+    cppbdd::MultiArgsCallableTask<int> given(
+        cppbdd::TaskName::GIVEN,
+        "a = {}",
+        [&](int x) { a = x; },
+        std::vector<cppbdd::MultiArgsCallableTask<int>::TestCase> {
+            cppbdd::MultiArgsCallableTask<int>::TestCase(1),
+        }
+    );
+    cppbdd::MultiArgsCallableTask<int> and_given(
+        cppbdd::TaskName::AND,
+        "b = {}",
+        [&](int x) { b = x; },
+        std::vector<cppbdd::MultiArgsCallableTask<int>::TestCase> {
+            cppbdd::MultiArgsCallableTask<int>::TestCase(3),
+        }
+    );
+    cppbdd::CallableTask when(
+        cppbdd::TaskName::WHEN,
+        "let c = a + b", [&](void) { c = a + b; }
+    );
+    cppbdd::MultiArgsCallableTask<int> then(
+        cppbdd::TaskName::THEN,
+        "c = {}",
+        [&](int x) { res = (c == x); },
+        std::vector<cppbdd::MultiArgsCallableTask<int>::TestCase> {
+            cppbdd::MultiArgsCallableTask<int>::TestCase(4),
+        }
+    );
+    manager.addTask(given);
+    manager.addTask(and_given);
+    manager.addTask(when);
+    manager.addTask(then);
+
+    manager.runAll();
+
+    EXPECT_EQ(a, 1);
+    EXPECT_EQ(b, 3);
+    EXPECT_EQ(c, 4);
+    EXPECT_TRUE(res);
+}
